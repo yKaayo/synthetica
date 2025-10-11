032f18b11daf9500ac0cfedada1addbaf355f069
Caio <caiokaea@gmail.com>
2025-10-11 20:20:49 -0300

    fix: change the origin

--- server/main.py
+++ server/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://synthetica-kaayo.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"]
 )
